2fd10050835b713b1b51c77989d8366b0eb37f53
iStair <ceo@browsersound.de>
2014-01-31 20:27:17 +0100

    create and use logfiles

--- iPHPLib/Natives/Log.php
+++ iPHPLib/Natives/Log.php
@@ -1,1 +1,52 @@
-test
+<?php
+
+/**
+* iPHPLib v0.1
+* @author Andre Liebich
+* @copyright Andre Liebich
+* @type create and use logs
+* @access public 
+*/
+
+/**
+* Log-class
+*/
+ 
+class Log
+{
+	
+	/**
+	* Add a line in the Log
+	* @params $m Message
+	*/
+	
+	public function add($m){
+		$u = new Config; // get info from config.php
+		$file = fopen($u->log_save_dir."/".date("d-m-Y").".txt", "a"); // create if not exists
+		$m = date("d-m-Y H:i:s")." - ".$m."\r\n";  
+		if(fputs($file, $m)){
+		  return true; // return true on success
+		}else{
+		  return false; // return false on error
+		}
+		fclose($file);
+	}
+	
+	/**
+	* Delete a Log
+	* @return true on success
+	* @return false on cancelled
+	*/
+	
+	public function del($f){
+		if(file_exists($f)){
+			unlink($f);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
+
+?>
