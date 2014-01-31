9f2b84afe6572e95ad2e275070ab903bf4674278
iStair <ceo@browsersound.de>
2014-01-31 20:20:19 +0100

    how do you use our PHP-library

---
+++ iphplib.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* Example-Code to use the Lib.
+*/
+
+require_once 'iPHPLib/LibLoader.php'; // Include PHP-Lib
+
+$iPHPLib = new iPHPLib; // load Lib 
+
+$iPHPLib->log->add("hellO"); // New Log with line
+
+//$iPHPLib->log->del("pathtolog.txt");
+
+?>
