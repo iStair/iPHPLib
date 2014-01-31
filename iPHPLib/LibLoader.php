55dcbaa3a6438463373c4f510542304db73485ec
iStair <ceo@browsersound.de>
2014-01-31 20:21:40 +0100

    loading the PHP-library

---
+++ iPHPLib/LibLoader.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* iPHPLib v0.1
+* @author Andre Liebich
+* @copyright Andre Liebich
+* @type main-class
+* @access public 
+*/
+
+/**
+* Use Autoload
+*/
+
+try {
+	if(file_exists(__DIR__."/Natives/Autoload.php")){
+		require_once __DIR__."/Natives/Autoload.php";
+	}else{
+		throw new Exception("Autoload can't load."); // Output Error 
+	}
+}catch(Exception $e){
+	echo $e->getMessage();
+}
+
+/**
+* main-class
+*/
+
+class iPHPLib
+{
+
+	/**
+	* all public vars
+	*/
+	
+	public $log;
+	
+	/**
+	* Initializing
+	*/
+	
+	function __construct(){
+		$this -> log = new Log; // Loading Log
+	}
+
+}
+
+?>
